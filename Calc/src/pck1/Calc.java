995a28d01b0331a74074a4cd02b2b4ce4d038ede
umangyadav <umang.work82@gmail.com>
2022-02-23 18:54:30 +0530

    Calc added

---
+++ Calc/src/pck1/Calc.java
@@ -0,0 +1,11 @@
+package pck1;
+
+public class Calc {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println("Sum");
+        System.out.println("Diff");
+        System.out.println("Mult");
+    }
+}
